d7b661c4fc18fc3191d86643b93b7a8278b64114
jskdr <jamessungjin.kim@gmail.com>
2024-07-08 18:00:37 -0700

    mistral serve

--- serve-quickstart/api-test_mistral.py
+++ serve-quickstart/api-test_mistral.py
@@ -5,10 +5,10 @@ from openai import OpenAI
 
 parser = argparse.ArgumentParser()
 # parser.add_argument("--base-url", default="https://api.openai.com")
-parser.add_argument("--base-url", default="https://model-service-gateway-ut381j39ahb2.oregon.google-cluster.vessl.ai")
+parser.add_argument("--base-url", default="https://model-service-gateway-ah60lx61f7b6.oregon.google-cluster.vessl.ai")
 parser.add_argument("--api-key", default="token-abc123")
-parser.add_argument("--model-name", default="casperhansen/llama-3-8b-instruct-awq")
-parser.add_argument("--prompt", default="Who are you?")
+parser.add_argument("--model-name", default="JamesKim/mistral-7b-qlora-alpaca-1k-0509_ARC-Train")
+parser.add_argument("--prompt", default="당신은 누구입니까?")
 args = parser.parse_args()
 
 client = OpenAI(
@@ -19,7 +19,7 @@ client = OpenAI(
 completion = client.chat.completions.create(
     model=args.model_name,
     messages=[
-        {"role": "system", "content": "Translate into English."},
+        # {"role": "system", "content": "Translate into English."},
         {"role": "user", "content": args.prompt},
     ],
 )
