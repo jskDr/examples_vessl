d19d4212e616511a6a50283a2b78c21aec357b5d
jskdr <jamessungjin.kim@gmail.com>
2024-07-31 18:46:16 -0700

    api-test

--- serve-quickstart/api-test_ragas.py
+++ serve-quickstart/api-test_ragas.py
@@ -5,9 +5,9 @@ from openai import OpenAI
 
 parser = argparse.ArgumentParser()
 # parser.add_argument("--base-url", default="https://api.openai.com")
-parser.add_argument("--base-url", default="https://model-service-gateway-ut381j39ahb2.oregon.google-cluster.vessl.ai")
+parser.add_argument("--base-url", default="https://model-service-gateway-a5vw2t543kme.oregon.google-cluster.vessl.ai")
 parser.add_argument("--api-key", default="token-abc123")
-parser.add_argument("--model-name", default="casperhansen/llama-3-8b-instruct-awq")
+parser.add_argument("--model-name", default="explodinggradients/Ragas-critic-llm-Qwen1.5-GPTQ")
 parser.add_argument("--prompt", default="나는 한글을 사랑해요.")
 args = parser.parse_args()
 
